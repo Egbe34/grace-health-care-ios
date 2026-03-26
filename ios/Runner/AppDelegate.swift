b6e16a2274655f3caf4593a9dc44c0b6b2bac448
Egbe Grace <graceegbe3@gmail.com>
2026-03-26 19:13:43 +0100

    Fix iOS pbxproj: add Firebase plist and remove SceneDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,18 +1,25 @@
 import UIKit
 import Flutter
-import FirebaseCore
 
-@main
+@UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    if FirebaseApp.app() == nil {
-      FirebaseApp.configure()
-    }
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-}
+}import UIKit
+ import Flutter
+
+ @UIApplicationMain
+ @objc class AppDelegate: FlutterAppDelegate {
+   override func application(
+     _ application: UIApplication,
+     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+   ) -> Bool {
+     GeneratedPluginRegistrant.register(with: self)
+     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+   }
+ }
